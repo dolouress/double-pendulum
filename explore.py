a6f9eacb0c62fe498a9d678e9b1a2bf903af467b
Mia <miamiletic.mm@gmail.com>
2024-06-14 18:12:38 +0200

    Added scatter plots for each relationship between thetas

--- explore.py
+++ explore.py
@@ -7,7 +7,7 @@ from acrobot_used import AcrobotEnv
 import pade
 
 # Initialize the Acrobot environment
-env = AcrobotEnv(render_mode='human')
+env = AcrobotEnv()
 env.reset()
 
 # Parameters for data collection
@@ -16,7 +16,9 @@ samples = 900  # Number of samples
 
 # Data storage
 plot_theta1 = np.zeros(samples)
+plot_dtheta1 = np.zeros(samples)
 plot_theta2 = np.zeros(samples)
+plot_dtheta2 = np.zeros(samples)
 plot_torque = np.zeros(samples)
 
 data = np.zeros((samples, 3))
@@ -49,6 +51,8 @@ for i in range(samples):
     # Store the data
     plot_theta1[i] = np.degrees(theta1)
     plot_theta2[i] = np.degrees(theta2)
+    plot_dtheta1[i] = np.degrees(dtheta1)
+    plot_dtheta2[i] = np.degrees(dtheta2)
     plot_torque[i] = torque
 
     data[i, 0] = theta1
@@ -58,20 +62,45 @@ for i in range(samples):
     target_dtheta2[i] = delta_dtheta2
 
 # Visualize the data
-scatter = plt.scatter(plot_theta1, plot_theta2, c=plot_torque, cmap='viridis', vmin=-1, vmax=1)
+scatter_th1th2 = plt.scatter(plot_theta1, plot_theta2, c=plot_torque, cmap='viridis', vmin=-1, vmax=1)
+plt.title("Theta1 vs. Theta2 with Torque Color Map")
 plt.colorbar(ticks=[-1, 0, 1])
+plt.xticks([-180, -90, 0, 90, 180])
+plt.yticks([-180, -90, 0, 90, 180])
+plt.xlabel(r'$\theta_1$ (degrees)')
+plt.ylabel(r'$\theta_2$ (degrees)')
 plt.show()
+
+scatter_th1dth1 = plt.scatter(plot_theta1, plot_dtheta1, c=plot_torque, cmap='viridis', vmin=-1, vmax=1)
+plt.title("Theta1 vs. dTheta1 with Torque Color Map")
+plt.colorbar(ticks=[-1, 0, 1])
+plt.xticks([-180, -90, 0, 90, 180])
+plt.yticks([-360, 0, 360])
+plt.xlabel(r'$\theta_1$ (degrees)')
+plt.ylabel(r'$\dot{\theta}_1$ (degrees/s)')
+plt.show()
+
+scatter_th2dth2 = plt.scatter(plot_theta1, plot_dtheta2, c=plot_torque, cmap='viridis', vmin=-1, vmax=1)
+plt.title("Theta1 vs. dTheta2 with Torque Color Map")
+plt.colorbar(ticks=[-1, 0, 1])
+plt.xticks([-180, -90, 0, 90, 180])
+plt.yticks([-360, 0, 360])
+plt.xlabel(r'$\theta_1$ (degrees)')
+plt.ylabel(r'$\dot{\theta}_2$ (degrees/s)')
+plt.show()
+
 
 # Train the qualitative model using PADE
 q_table = pade.pade(data, target_dtheta1, nNeighbours=10)
 q_labels = pade.create_q_labels(q_table[:, 2:3], ['torque'])
+
 classes, class_names = pade.enumerate_q_labels(q_labels)
 
-classifier = tree.DecisionTreeClassifier(min_impurity_decrease=0.05)
+classifier = tree.DecisionTreeClassifier(min_impurity_decrease=0.005)
 model = classifier.fit(data[:, 0:2], classes)
 
-# Print the decision tree rules
-tree_rules = export_text(model, feature_names=['theta1', 'theta2'])
+# Print the decision tree rules for theta 1
+tree_rules = export_text(model, feature_names=['theta1', 'dtheta1'])
 print(tree_rules)
 
 # Convert threshold values from radians to degrees
@@ -79,6 +108,6 @@ for i, threshold in enumerate(model.tree_.threshold):
     if threshold != tree._tree.TREE_UNDEFINED:
         model.tree_.threshold[i] = np.degrees(threshold)
 
-# Visualize the decision tree
-tree.plot_tree(model, feature_names=['theta1', 'theta2'], class_names=class_names, filled=True)
+# Visualize the decision tree for theta 1
+tree.plot_tree(model, feature_names=['theta1', 'dtheta1'], class_names=class_names, filled=True)
 plt.show()
